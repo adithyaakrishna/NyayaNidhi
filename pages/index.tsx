5c41b44e97af6c88389ca88d766e606dc3f8e09e
Adithya Krishna <aadithya794@gmail.com>
2023-11-21 02:19:27 +0530

    chore: updated content and images
    Signed-off-by: Adithya Krishna <aadithya794@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ const IndexPage = () => (
   <Layout title="Our advocates and lawyers are working behind the scenes, we will come with the judgement soon :)">
     <img
       src="/images/xera.svg"
-      alt="Themeptation "
+      alt="Themeptation"
       className="absolute h-96 -top-20 -right-16 lg:right-5 lg:top-10 animate-blob"
     />
     <img
@@ -15,18 +15,25 @@ const IndexPage = () => (
     />
     <div className="relative z-10 py-6 space-y-16 lg:space-y-32 text-gray-900">
       <div className="text-center space-y-10">
-        <h3 className="font-light text-xl uppercase tracking-wider">
-          Judging soon
+        <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <h1 className="text-7xl lg:text-9xl font-extrabold" style={{marginRight: "50px"}}>NyayaNidhi</h1>
+        <img
+          src="/images/NN.png"
+          alt="hero"
+          style={{width: "15%"}}
+        />
+        </div>
+        <h3 className="font-light text-xl tracking-wider">
+          10x Productivity of Indian Legal System
         </h3>
-        <h1 className="text-7xl lg:text-9xl font-extrabold">10X Productivity of Indian Legal System</h1>
-        <p className="text-xl lg:text-2xl tracking-wide mx-10 lg:max-w-xl lg:mx-auto">
-          Our advocates and lawyers are working behind the scenes, we will come with the judgement soon :)
+        {/* <h1 className="text-7xl lg:text-9xl font-extrabold">10X Productivity of Indian Legal System</h1> */}
+        <p className="text-xl lg:text-2xl tracking-wide mx-10 ">
+          Our advocates and lawyers are working behind the scenes :)
         </p>
       </div>
     </div>
     <div className="absolute bottom-5">
       <ul className="flex space-x-3">
-        
         <li>
           <Link href="https://twitter.com/nyayanidhi">
             <a className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-50 transition ease-in-out duration-500 hover:bg-red-500 hover:text-white hover:shadow-lg">
